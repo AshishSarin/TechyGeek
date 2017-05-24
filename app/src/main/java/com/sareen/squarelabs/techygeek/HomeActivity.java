c241fbd3822b3103f04aaf9ab7e5a1e416024714
AshishSarin <asareen007@gmail.com>
2017-05-25 00:33:00 +0530

    Added Home Activity

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.sareen.squarelabs.techygeek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
