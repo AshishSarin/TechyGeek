f1a6c298e4069c1a0d1684991d439cea8c69d906
AshishSarin <asareen007@gmail.com>
2017-05-28 04:26:05 +0530

    Post model added

--- app/src/main/java/com/sareen/squarelabs/techygeek/model/Post.java
+++ app/src/main/java/com/sareen/squarelabs/techygeek/model/Post.java
@@ -6,4 +6,40 @@ package com.sareen.squarelabs.techygeek.model;
 
 public class Post
 {
+    String postId;
+    String mainImageUrl;
+    String title;
+    String text;
+
+    public Post()
+    {
+    }
+
+    public Post(String postId, String mainImageUrl, String title, String text)
+    {
+        this.postId = postId;
+        this.mainImageUrl = mainImageUrl;
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getPostId()
+    {
+        return postId;
+    }
+
+    public String getMainImageUrl()
+    {
+        return mainImageUrl;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
 }
