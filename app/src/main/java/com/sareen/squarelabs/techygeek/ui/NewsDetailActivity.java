279a57a75e6d57e163063c5946d33e449dee5350
ashish <asareen007@gmail.com>
2017-06-03 16:41:24 +0530

    Activity for showing detail of news added

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/ui/NewsDetailActivity.java
@@ -0,0 +1,15 @@
+package com.sareen.squarelabs.techygeek.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sareen.squarelabs.techygeek.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
