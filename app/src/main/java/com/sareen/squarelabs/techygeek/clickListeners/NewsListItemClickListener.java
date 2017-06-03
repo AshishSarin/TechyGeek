7d63109c0e3f9fc32af7dfb5cf17b44634f744b6
ashish <asareen007@gmail.com>
2017-06-03 16:34:51 +0530

    Item click listener for recyler view of news list

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/clickListeners/NewsListItemClickListener.java
@@ -0,0 +1,12 @@
+package com.sareen.squarelabs.techygeek.clickListeners;
+
+import android.view.View;
+
+/**
+ * Created by ashish on 6/3/17.
+ */
+
+public interface NewsListItemClickListener
+{
+    public void onItemClick(View view, int position);
+}
