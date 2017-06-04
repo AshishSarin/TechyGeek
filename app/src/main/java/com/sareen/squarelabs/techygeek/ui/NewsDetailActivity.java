909cc6e848036eb76442b867cedf2eaa7cf671e3
ashish <asareen007@gmail.com>
2017-06-04 12:52:57 +0530

    News Detail Activity made child of Tech Geek Activity

--- app/src/main/java/com/sareen/squarelabs/techygeek/ui/NewsDetailActivity.java
+++ app/src/main/java/com/sareen/squarelabs/techygeek/ui/NewsDetailActivity.java
@@ -1,21 +1,34 @@
 package com.sareen.squarelabs.techygeek.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.sareen.squarelabs.techygeek.R;
 
-public class NewsDetailActivity extends AppCompatActivity
+public class NewsDetailActivity extends TechyGeekActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news_detail);
+        FrameLayout mFrameLayout = (FrameLayout)findViewById(R.id.layout_base);
+        LayoutInflater mLayoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View detailActivityView = mLayoutInflater.inflate(R.layout.activity_news_detail, null, false);
+        mFrameLayout.addView(detailActivityView);
+
+        //Setting up the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Detail Activity");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(null);
+
         String detailText = getIntent().getStringExtra("detailText");
-        TextView detailTextView = (TextView)findViewById(R.id.detail_text);
+        TextView detailTextView = (TextView)detailActivityView.findViewById(R.id.detail_text);
         detailTextView.setText(detailText);
     }
 }
