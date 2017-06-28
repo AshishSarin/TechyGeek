15a35c560c8562d21decdeca74db5e713d2d090e
ashish <asareen007@gmail.com>
2017-06-29 02:36:33 +0530

    Detail Activity Created

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/utilities/Utility.java
@@ -0,0 +1,20 @@
+package com.sareen.squarelabs.techygeek.utilities;
+
+/**
+ * Created by ashish on 6/13/17.
+ */
+
+public class Utility
+{
+    // Constants used for passing data
+    public static final String DETAIL_TTITLE = "detailTitle";
+    public static final String DETAIL_TEXT = "detailText";
+    public static final String DETAIL_POSTID = "detailPostID";
+    public static final String DETAIL_IMAGE = "detailImage";
+
+
+    // Constant for determining activity which calle detail activity
+    public static final String DETAIL_CALLING_ACTIVITY = "calling_activity";
+
+
+}
