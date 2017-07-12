0e4f77028039d96247b290c307affed34cdd37b2
ashish <asareen007@gmail.com>
2017-07-13 00:05:18 +0530

    Content Provider class for Techy Geek

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/data/TechyGeekProvider.java
@@ -0,0 +1,12 @@
+package com.sareen.squarelabs.techygeek.data;
+
+import android.content.ContentProvider;
+
+/**
+ * Created by ashish on 13/7/17.
+ */
+
+public class TechyGeekProvider extends ContentProvider
+{
+
+}
