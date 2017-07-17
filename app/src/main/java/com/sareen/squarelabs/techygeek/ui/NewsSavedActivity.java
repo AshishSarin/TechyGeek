03c84612ca34793785871ba5b65affaf72acab8f
ashish <asareen007@gmail.com>
2017-07-18 00:04:23 +0530

    Activity for showing downloded posts added

---
+++ app/src/main/java/com/sareen/squarelabs/techygeek/ui/NewsSavedActivity.java
@@ -0,0 +1,20 @@
+package com.sareen.squarelabs.techygeek.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.sareen.squarelabs.techygeek.R;
+
+public class NewsSavedActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_saved);
+    }
+}
